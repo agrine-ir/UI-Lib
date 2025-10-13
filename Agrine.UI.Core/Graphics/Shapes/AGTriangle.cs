b32574262932c7b9cc816719b7f139ae88262663
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-10-13 22:48:16 +0330

    - Update AGTriangle Base Code

--- Agrine.UI.Core/Graphics/Shapes/AGTriangle.cs
+++ Agrine.UI.Core/Graphics/Shapes/AGTriangle.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Agrine.UI.Core.Graphics.Shapes
 {
-    public class AGTriangle
+    public class AGTriangle : ShapeBase
     {
+        public AGTriangle(Point location, Size size)
+           : base(location, size) { }
 
+        public override void Draw(System.Drawing.Graphics g)
+        {
+            Point p1 = new Point(Location.X + Size.Width / 2, Location.Y);
+            Point p2 = new Point(Location.X, Location.Y + Size.Height);
+            Point p3 = new Point(Location.X + Size.Width, Location.Y + Size.Height);
+
+            Point[] points = { p1, p2, p3 };
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                       new Rectangle(Location, Size), FillColor1, FillColor2, GradientMode))
+            using (Pen pen = new Pen(BorderColor, BorderThickness))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.FillPolygon(brush, points);
+                g.DrawPolygon(pen, points);
+            }
+        }
+
+        public override bool Contains(Point p)
+        {
+            Point p1 = new Point(Location.X + Size.Width / 2, Location.Y);
+            Point p2 = new Point(Location.X, Location.Y + Size.Height);
+            Point p3 = new Point(Location.X + Size.Width, Location.Y + Size.Height);
+            Point[] pts = { p1, p2, p3 };
+
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.AddPolygon(pts);
+                return path.IsVisible(p);
+            }
+        }
     }
 }
