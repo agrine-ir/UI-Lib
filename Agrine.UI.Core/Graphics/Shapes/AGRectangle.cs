0a6bdf1777acadb61b8c943cdb809a60019ce280
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-10-13 22:37:40 +0330

    - Update AGRectangle Class Base Code

--- Agrine.UI.Core/Graphics/Shapes/AGRectangle.cs
+++ Agrine.UI.Core/Graphics/Shapes/AGRectangle.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Agrine.UI.Core.Graphics.Shapes
 {
-    public class AGRectangle
+    public class AGRectangle : ShapeBase
     {
+        public AGRectangle(Point location, Size size)
+           : base(location, size) { }
 
+        public override void Draw(Graphics g)
+        {
+            Rectangle rect = new Rectangle(Location, Size);
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(rect, FillColor1, FillColor2, GradientMode))
+            using (Pen pen = new Pen(BorderColor, BorderThickness))
+            using (GraphicsPath path = GetRoundedRect(rect, CornerRadius))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.FillPath(brush, path);
+                g.DrawPath(pen, path);
+            }
+        }
+
+        public override bool Contains(Point p)
+        {
+            Rectangle rect = new Rectangle(Location, Size);
+            using (GraphicsPath path = GetRoundedRect(rect, CornerRadius))
+            {
+                return path.IsVisible(p);
+            }
+        }
     }
 }
