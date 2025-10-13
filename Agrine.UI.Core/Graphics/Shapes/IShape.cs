78d45ce0a87067890c39b0b7842beaac3c49e384
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-10-13 22:34:03 +0330

    - Add and Update IShape Interface

---
+++ Agrine.UI.Core/Graphics/Shapes/IShape.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Agrine.UI.Core.Graphics.Shapes
+{
+    public interface IShape
+    {
+        Point Location { get; set; }
+        Size Size { get; set; }
+
+        Color FillColor1 { get; set; }
+        Color FillColor2 { get; set; }
+        LinearGradientMode GradientMode { get; set; }
+
+        Color BorderColor { get; set; }
+        float BorderThickness { get; set; }
+        float CornerRadius { get; set; }
+
+        void Draw(Graphics g);
+        bool Contains(Point p);
+        void Move(int dx, int dy);
+        void Resize(Size newSize);
+    }
+}
