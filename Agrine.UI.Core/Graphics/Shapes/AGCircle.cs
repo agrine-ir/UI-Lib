cf866da9df319370f7112edd87d0fce639fbd965
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-10-13 22:39:39 +0330

    - Update AGCircle Class Base Code

--- Agrine.UI.Core/Graphics/Shapes/AGCircle.cs
+++ Agrine.UI.Core/Graphics/Shapes/AGCircle.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Agrine.UI.Core.Graphics.Shapes
 {
-    public class AGCircle
+    public class AGCircle : ShapeBase
     {
+        public AGCircle(Point location, Size size)
+            : base(location, size) { }
 
+        public override void Draw(Graphics g)
+        {
+            Rectangle rect = new Rectangle(Location, Size);
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(rect, FillColor1, FillColor2, GradientMode))
+            using (Pen pen = new Pen(BorderColor, BorderThickness))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.FillEllipse(brush, rect);
+                g.DrawEllipse(pen, rect);
+            }
+        }
+
+        public override bool Contains(Point p)
+        {
+            float centerX = Location.X + Size.Width / 2f;
+            float centerY = Location.Y + Size.Height / 2f;
+            float radius = Size.Width / 2f;
+
+            float dx = p.X - centerX;
+            float dy = p.Y - centerY;
+
+            return dx * dx + dy * dy <= radius * radius;
+        }
     }
 }
